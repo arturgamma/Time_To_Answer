3549c35758f3cd16a98cd2a6903621ddf2d0c9c8
Artur Gamma <arturgamma@gmail.com>
2025-06-29 21:20:13 -0300

    Inclusão de assunto por questão, e filtro pro ele

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,12 @@
 module SiteHelper
+    def msg_jumbotrom
+        case params[:action]
+        when 'index'
+            "Últimas perguntas cadastradas"
+        when 'questions'
+            "Resultados para \"#{params[:term]}\""
+        when 'subject'
+            "Mostrando questôes do assunto \"#{params[:subject]}\""
+        end    
+    end
 end
