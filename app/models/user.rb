69f007b53c2cef4c6179651570f12a7171ba75f4
Artur Gamma <arturgamma@gmail.com>
2025-10-17 21:04:30 -0300

    inclusão de validates para first_name user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  validates :first_name, presence: true, length: { minimum: 4}, on: :update
+  
+         #virtual attributes
   def full_name  
    [first_name, last_name].join(' ')
   end  
