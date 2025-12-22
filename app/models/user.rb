fe4d250039530b9bd3685856a22229dcd883a4ad
Artur Gamma <arturgamma@gmail.com>
2025-12-22 13:08:33 -0300

    inclusão de novos campos para perfil de usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+       devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+       has_one :user_profile
+       accepts_nested_attributes_for :user_profile, reject_if: :all_blank
+
   validates :first_name, presence: true, length: { minimum: 4}, on: :update
   
          #virtual attributes
